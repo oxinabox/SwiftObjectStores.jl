54fb4ea06172dd09b9caf2207d7fae941c0a2a09
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-09-09 16:57:54 +1000

    =Be more helpful to people not running pycall via conda python

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,30 @@
 
-
+using PyCall
 using Conda
 
-Conda.add("pip")
-pip = joinpath(Conda.BINDIR, "pip")
-run(`$pip install python-keystoneclient`)
-run(`$pip install python-swiftclient`)
 
+if PyCall.conda
+	Conda.add("pip")
+	pip = joinpath(Conda.BINDIR, "pip")
+	run(`$pip install python-keystoneclient`)
+	run(`$pip install python-swiftclient`)
+else
+	try
+		pyimport("keystoneclient")
+		pyimport("swiftclient")
+	catch ee
+		typeof(ee) <: PyCall.PyError || rethrow(ee)
+		warn("""
+Python Dependancies not installed
+Please either:
 
+ - Rebuild PyCall to use Conda, by running in the julia REPL:
+    - `ENV[PYTHON]=""; Pkg.build("PyCall"); Pkg.build("SwiftObjectStores")`
+ - Or install the depencences, eg by running pip
+    - `pip install python-keystoneclient`
+	- `pip install python-swiftclient`
+	"""
+		)
+	end
+
+end
