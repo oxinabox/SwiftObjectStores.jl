9628c84cd018c20502ecd85d4c4d6e40782bf6fa
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-11-18 22:25:52 +1100

    =Keeping object name as filename -- so as to preserve the file extentsion, which may encode useful information for FileIO.jl

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 [![Coverage Status](https://coveralls.io/repos/oxinabox/SwiftObjectStores.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/oxinabox/SwiftObjectStores.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/oxinabox/SwiftObjectStores.jl/coverage.svg?branch=master)](http://codecov.io/github/oxinabox/SwiftObjectStores.jl?branch=master)
+
+>Swift is a highly available, distributed, eventually consistent object/blob store. Organizations can use Swift to store lots of data efficiently, safely, and cheaply.
+
+
+Makes [Open Stack Swift Object Stores](http://docs.openstack.org/developer/swift/) available from within Julia.
+This package wraps the Python Swift Client, but makes significant changes in terms of interface.
+
+
+
+
--- src/download.jl
+++ src/download.jl
@@ -5,14 +5,14 @@
 export get_file, get_stream, get_jld
 
 """Download from swift, writing the result to the file given by `fname`.
-	`container` is just the container name, pseudofolder should be part of object name
+	`container` is just the container object_name, pseudofolder should be part of object object_name
 """
-function get_file(serv, container::String, name::String, fname::String; verbose::Bool=false)
+function get_file(serv, container::String, object_name::String, fname::String; verbose::Bool=false)
 	if '/' in container || '\\' in container
-		error("No slashes allowed in container name (got $container). Pseudodir should be part of the object name.")
+		error("No slashes allowed in container object_name (got $container). Pseudodir should be part of the object object_name.")
 	end
 		
-    async_get = serv[:download](container, [name], Dict("out_file" => fname))
+    async_get = serv[:download](container, [object_name], Dict("out_file" => fname))
     responses=collect(async_get)
     if verbose
         show_responses(responses)
@@ -27,24 +27,25 @@ end
 """Download a file from Swift, apply `func` to it, returning the result.
 `func` should be a function that takes a filename as an input.
 """
-function get_file(func::Function, serv, container::String, name::String; verbose::Bool=false)
-    mktempdir() do tdir
-        fname = joinpath(tdir, "lastdownswift")
-        get_file(serv, container, name, fname; verbose=verbose)    
-        func(fname)
+function get_file(func::Function, serv, container::String, object_name::String; verbose::Bool=false)
+	fname = splitdir(object_name)[end]
+	mktempdir() do tdir
+	    fpathname = joinpath(tdir, fname)
+        get_file(serv, container, object_name, fpathname; verbose=verbose)    
+        func(fpathname)
     end
 end
 
 """Read IO stream, from Swift, and call func, on it, returning the result """
-function get_stream(func::Function, serv, container::String, name::String; verbose::Bool=false)
-    get_file(serv, container, name; verbose=verbose) do fname
+function get_stream(func::Function, serv, container::String, object_name::String; verbose::Bool=false)
+    get_file(serv, container, object_name; verbose=verbose) do fname
 		open(func, fname, "r")
     end
 end
 
 """Download a JLD file from Swift. `dataname` is a list of fieldnames to read."""
-function get_jld(serv, container::String, name::String, datanames...; verbose::Bool=false)
-    get_file(serv, container, name; verbose=verbose) do fname
+function get_jld(serv, container::String, object_name::String, datanames...; verbose::Bool=false)
+    get_file(serv, container, object_name; verbose=verbose) do fname
 		JLD.load(File(format"JLD", fname), datanames...)
     end
 end
