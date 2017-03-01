038e595bf3cbbcf08e8932a3fd3294da90016fb0
Lyndon White <oxinabox@ucc.asn.au>
2017-03-01 02:44:20 +0000

    =debug code to list file on windows

--- deps/build.jl
+++ deps/build.jl
@@ -4,8 +4,17 @@ using Conda
 
 if PyCall.conda
 	Conda.add("pip")
+    
+    #Debug ode, where *is* pip?
+    for (root, dirs, files) in walkdir(".")
+      println("Files in $root")
+      for file in files
+          println(joinpath(root, file)) # path to files
+      end
+    end
+
 	pip = joinpath(Conda.PYTHONDIR, "pip")
-	run(`$pip install python-keystoneclient`)
+    run(`$pip install python-keystoneclient`)
 	run(`$pip install python-swiftclient`)
 else
 	try
